7b114897350d510d47036b8e44a79e8624e7807b
svrem125 <svrem@icloud.com>
2021-03-23 17:02:25 +0100

    visual changes

--- renderer/pages/next.jsx
+++ renderer/pages/next.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 import cookieCutter from "cookie-cutter";
 import { useRouter } from "next/router";
+import styles from "../style/next.module.css";
 
 const Next = () => {
   let url = "";
   const [bad, setBad] = useState(false);
+  const [messages, setMessages] = useState([
+    { send: false, message: "hello world" },
+  ]);
+  const [status, setStatus] = useState("Disconnected");
+
   const router = useRouter();
 
   useEffect(() => {
@@ -14,16 +20,13 @@ const Next = () => {
   }, [bad]);
 
   if (!bad) {
-    // console.log("");
-    // router.push("/home");
-
     try {
       url = cookieCutter.get("url");
     } catch {}
-    console.log(url);
     if (url !== "") {
       try {
         const ws = new WebSocket(url);
+        if (status === "Disconnected") setStatus("Connected");
       } catch {
         cookieCutter.set("errorMessage", "Couldn't connect to the WebSocket.");
         console.log(cookieCutter.get("errorMessage"));
@@ -31,7 +34,21 @@ const Next = () => {
         setBad(true);
       }
 
-      return <h1>{url}</h1>;
+      return (
+        <main className={styles.main}>
+          <div className={styles.statusContainer}>
+            <h1>Status: {status}</h1>
+            <h3>{url}</h3>
+          </div>
+          <div className={styles.messages}></div>
+          <form className={styles.inputContainer}>
+            <button className={styles.disconnect}>Disconnect</button>
+
+            <input placeholder="Message" />
+            <button className={styles.send}>Send</button>
+          </form>
+        </main>
+      );
     } else {
       setBad(true);
     }
